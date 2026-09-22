6ffa55a17b35c382ff9da8ba5adbed2e2db3f500
Claxid <hohojiji651@gmail.com>
2026-09-22 09:26:34 +0200

    header

--- StarWars/src/App.tsx
+++ StarWars/src/App.tsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Header from './Components/Header'
 import Home from './pages/Home.tsx'
 import Characters from './pages/Characters.tsx'
 
 function App() {
   return (
     <>
-      <h1>StarWars 1</h1>
-      <button>Home</button>
+      <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/characters" element={<Characters />} />
       </Routes>
